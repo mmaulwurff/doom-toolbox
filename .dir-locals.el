7f1dbd473f4f906411403e341f9a483655e419bc
Alexander Kromm <mmaulwurff@gmail.com>
2025-02-11 20:12:43 +0700

    fix licensing

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
+;; SPDX-FileCopyrightText: © 2025 Alexander Kromm <mmaulwurff@gmail.com>
+;; SPDX-License-Identifier: CC0-1.0
+
 ((nil . ((ispell-personal-dictionary . "./config/aspell.en.pws")
          (ispell-local-dictionary . "american"))))
