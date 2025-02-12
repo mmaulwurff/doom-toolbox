c93bef0d6b107f013a2b2adbd90db10f0a6cbef8
Alexander Kromm <mmaulwurff@gmail.com>
2025-02-12 08:43:26 +0700

    add links to tools

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ;; SPDX-FileCopyrightText: © 2025 Alexander Kromm <mmaulwurff@gmail.com>
 ;; SPDX-License-Identifier: CC0-1.0
 
+;; This is directory local variables file for Emacs.
+
 ((nil . ((ispell-personal-dictionary . "./config/aspell.en.pws")
          (ispell-local-dictionary . "american"))))
