c5f18d0d34d6baae04ba90c75ebf36056eef9c94
Alexander Kromm <mmaulwurff@gmail.com>
2025-06-11 23:09:08 +0700

    custom: rewrite

--- .dir-locals.el
+++ .dir-locals.el
@@ -8,7 +8,7 @@
          ;; See https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
          (eval . (setq ispell-personal-dictionary
                        (expand-file-name
-                        "./config/aspell.en.pws"
+                        "./tools/aspell.en.pws"
                         (file-name-directory
                          (let ((d (dir-locals-find-file "./")))
                            (if (stringp d) d (car d)))))))
