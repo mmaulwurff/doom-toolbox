50eaaf5365bab2c833737ce6065b79e9997afe59
Alexander Kromm <mmaulwurff@gmail.com>
2025-08-05 09:21:56 +0700

    custom: try to make html output more readable

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,12 +4,29 @@
 ;; This is directory local variables file for Emacs.
 
 ((nil . (
-         ;; Set the path to the dictionary file so it works in files in subdirectories too.
-         ;; See https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
-         (eval . (setq ispell-personal-dictionary
-                       (expand-file-name
-                        "./tools/aspell.en.pws"
-                        (file-name-directory
-                         (let ((d (dir-locals-find-file "./")))
-                           (if (stringp d) d (car d)))))))
-         (ispell-local-dictionary . "american"))))
+  ;; Set the path to the dictionary file so it works in files in subdirectories too.
+  ;; See https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
+  (eval . (setq ispell-personal-dictionary
+                (expand-file-name
+                 "./tools/aspell.en.pws"
+                 (file-name-directory
+                  (let ((d (dir-locals-find-file "./"))) (if (stringp d) d (car d)))))))
+  (ispell-local-dictionary . "american")))
+
+(org-mode . (
+  (org-html-doctype . "html5")
+  (org-html-html5-fancy . t)
+  ;; See https://emacs.stackexchange.com/questions/3374/set-the-background-of-org-exported-code-blocks-according-to-theme
+  (eval . (progn
+            (defun my/org-inline-css-hook (exporter)
+              "Insert custom inline CSS to set the code colors code according to the current theme"
+              (when (eq exporter 'html)
+                (let* ((my-pre-bg (face-background 'default))
+                       (my-pre-fg (face-foreground 'default)))
+                  (setq
+                   org-html-head-extra
+                   (concat
+                    org-html-head-extra
+                    (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                            my-pre-bg my-pre-fg))))))
+            (add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook))))))
--- tools/package.sh
+++ tools/package.sh
@@ -12,18 +12,20 @@
 title=$(grep --max-count=1 "#+title: " "$1" | sed "s/#+title: //" | sed "s/ /-/")
 name=$(basename "$1" ".org")
 
+# 1. Clean build directory, package directory, and package.
 rm --recursive --force "build/$name"
-
-emacs "$1" --quick --batch --eval "(progn (require 'ob-tangle) (setq org-confirm-babel-evaluate nil) (org-babel-tangle))"
-
 rm --recursive --force "build/$title"
 rm --recursive --force "build/$title.pk3"
 
+# 2. Tangle the source to build files.
+# TODO: export to HTML too.
+emacs "$1" --quick --batch --eval "(progn (require 'ob-tangle) (setq org-confirm-babel-evaluate nil) (org-babel-tangle))"
+
+# 3. Copy build files, licenses, and documentation to package directory.
 mkdir --parents "build/$title"
 cp --recursive "build/$name/"* "build/$title"
 cp --recursive LICENSES "build/$title"
 cp --recursive documentation "build/$title"
-cp "$1" "build/$title/Readme.org"
 
 cd "build/$title" || exit
 7z a -tzip "../$title.pk3" ./*
