200dd2d4ecbca9e30e791e04cbdc7bd263347675
Alexander Kromm <mmaulwurff@gmail.com>
2026-04-01 09:21:48 +0700

    project: build on Windows: check PATH

--- sconstruct.py
+++ sconstruct.py
@@ -47,6 +47,7 @@ assert emacs is not None
 
 uzdoom = which('uzdoom') or Path(path.expanduser('~')) / 'UZDoom.AppImage'
 print(f'Found UZDoom: {uzdoom}')
+print(f'PATH: {environ["PATH"]}')
 
 
 # Common functions
