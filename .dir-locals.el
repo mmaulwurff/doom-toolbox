38d2a74e48c557ae25e70201abe8f0827f4a8a18
Alexander Kromm <mmaulwurff@gmail.com>
2026-04-09 21:40:54 +0700

    project: remove obsolete HTML export configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -11,22 +11,4 @@
                  "./tools/aspell.en.pws"
                  (file-name-directory
                   (let ((d (dir-locals-find-file "./"))) (if (stringp d) d (car d)))))))
-  (ispell-local-dictionary . "american")))
-
-(org-mode . (
-  (org-html-doctype . "html5")
-  (org-html-html5-fancy . t)
-  ;; See https://emacs.stackexchange.com/questions/3374/set-the-background-of-org-exported-code-blocks-according-to-theme
-  (eval . (progn
-            (defun my/org-inline-css-hook (exporter)
-              "Insert custom inline CSS to set the code colors code according to the current theme"
-              (when (eq exporter 'html)
-                (let* ((my-pre-bg (face-background 'default))
-                       (my-pre-fg (face-foreground 'default)))
-                  (setq
-                   org-html-head-extra
-                   (concat
-                    org-html-head-extra
-                    (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
-                            my-pre-bg my-pre-fg))))))
-            (add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook))))))
+  (ispell-local-dictionary . "american"))))
